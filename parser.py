0aafad77c8b797e940d6b39272290720e5842490
AnandhKumar E S <anandhkumares@gmail.com>
2022-06-17 14:43:30 +0530

    Added a parser and a gitignore
    Added a parser using argparse. Currently, parser supports 4 arguments. Language [-l, --langauge] for language of project, Name [-n, --name] for name of the project, Type [-t, --type] for type of project (small/large), and finally Category [-c, --category] for category that project belongs to. Category support multiple value and other support only one value.

---
+++ parser.py
@@ -0,0 +1,50 @@
+import argparse
+
+args = [
+    '--language',
+    '--category',
+    '--name',
+    '--type',
+]
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="A project/program mananger")
+    parser.add_argument(
+        '-l',
+        '--language',
+        dest='language',
+        help='Tell mkproj language used by project/program',
+        type=str,
+        required=True,
+
+    )
+    parser.add_argument(
+        '-c',
+        '--category',
+        dest='categoryName',
+        help='Tell mkproj category for project/program',
+        type=str,
+        required=True,
+        nargs='+'
+
+    )
+    parser.add_argument(
+        '-n',
+        '--name',
+        dest='projName',
+        help='Tell mkproj projName for project/program',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '-t',
+        '--type',
+        dest='type',
+        help='Program type large or small. default [large]',
+        type=str,
+        required=False
+    )
+
+    return parser.parse_args()
